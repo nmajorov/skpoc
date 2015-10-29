ac1ea90d9590a3106b42e2311d4e3744d7b9b9ca
Nikolaj Majorov <nmajorov@redhat.com>
2015-10-29 19:05:28 +0100

    add simple http server in test

--- skyguide-ei-parent/fdp01/src/test/java/ch/zuehlke/poc/RouteTest.java
+++ skyguide-ei-parent/fdp01/src/test/java/ch/zuehlke/poc/RouteTest.java
@@ -6,20 +6,60 @@ import org.apache.camel.EndpointInject;
 import org.apache.camel.Produce;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.component.mock.MockEndpoint;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("/META-INF/spring/camel-context.xml")
 public class RouteTest extends TestCase {
+	
+	@SuppressWarnings("restriction")
+	private HttpServer localServer;
 
 	@EndpointInject(uri = "mock:result")
 	protected MockEndpoint result;
 
 	@Produce
 	protected ProducerTemplate template;
+	
+	@SuppressWarnings("restriction")
+	static class MyHandler implements HttpHandler {
+        public void handle(HttpExchange t) throws IOException {
+            String response = "<root><poc>skyguide</poc></root>";
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+	
+	@SuppressWarnings("restriction")
+	@Before
+	@Override
+	public void setUp() throws Exception {
+		HttpServer server;
+
+		server = HttpServer.create(new InetSocketAddress(8000), 0);
+
+		server.createContext("/test", new MyHandler());
+		server.setExecutor(null); // creates a default executor
+		server.start();
+
+		super.setUp();
+	}
 
 	@Test
 	public void testCustomerFuse() throws Exception {
