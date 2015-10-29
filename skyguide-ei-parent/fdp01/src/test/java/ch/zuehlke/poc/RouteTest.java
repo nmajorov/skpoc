9660fba4f2f6d5fd71e18150267b02b9d4f926b9
DevVM <dev@localhost.localdomain>
2015-10-29 09:54:17 -0700

    Cleanup

--- skyguide-ei-parent/fdp01/src/test/java/ch/zuehlke/poc/RouteTest.java
+++ skyguide-ei-parent/fdp01/src/test/java/ch/zuehlke/poc/RouteTest.java
@@ -3,42 +3,29 @@ package ch.zuehlke.poc;
 import junit.framework.TestCase;
 
 import org.apache.camel.EndpointInject;
-import org.apache.camel.Exchange;
 import org.apache.camel.Produce;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.component.mock.MockEndpoint;
-import org.apache.camel.test.spring.CamelSpringTestSupport;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.context.support.AbstractXmlApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("/META-INF/spring/camel-context.xml")
 public class RouteTest extends TestCase {
-	
-	 @EndpointInject(uri = "mock:result")
-	 protected MockEndpoint result;
 
-	 
-	 @Produce
-	 protected ProducerTemplate template;
-	 
-	 
-   
+	@EndpointInject(uri = "mock:result")
+	protected MockEndpoint result;
 
-    @Test
-    public void testCustomerFuse() throws Exception {
-    	
-    	String out = template.requestBody("netty-http:http://localhost:8088/foo","<root><poc>skyguide</poc></root>",String.class);
+	@Produce
+	protected ProducerTemplate template;
 
-    	 
-    	 
-       
-    	 assertEquals("<root><poc>skyguide</poc></root>", out);
+	@Test
+	public void testCustomerFuse() throws Exception {
+		String out = template.requestBody("start",
+				"<root><poc>skyguide</poc></root>", String.class);
 
-    }
+		assertEquals("<root><poc>skyguide</poc></root>", out);
+	}
 }
-
